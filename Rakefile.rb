7ed8654220c704704bf781bf066d514b03e542ee
Ethan Stryker <e.stryker@gmail.com>
2011-03-13 22:33:32 +0000

    added/updated Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -13,3 +13,16 @@ Rake::RDocTask.new do |rd|
   rd.options << "--all"
   rd.options << "--inline-source"
 end
+
+task :upload_docs => [:rdoc] do
+ sh %(scp -r html/* e_stryker@opt-simple.rubyforge.org:/var/www/gforge-projects/opt-simple)
+end
+
+task :release => [:rdoc] do 
+  sh %(gem build opt-simple.gemspec)
+  sh %(git push  gitosis@rubyforge.org:opt-simple.git master)
+  
+  # god I love ruby:
+  newest_gem = Dir["opt-simple-*.gem"].sort {|a,b| File.mtime(b) <=> File.mtime(a)}.first
+  sh "gem1.9.1 push #{newest_gem}"
+end
